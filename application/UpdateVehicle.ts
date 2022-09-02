4c01d135b0b50ade5449aab53a873d5fb7124fae
Fernando Souza <fernando.souza@doxacode.com.br>
2022-09-02 12:07:13 -0300

    feat: update vehicle

---
+++ application/UpdateVehicle.ts
@@ -0,0 +1,21 @@
+import { VehicleProps } from "../domain/entities/Vehicle";
+import { VehiclesRepository } from "../domain/repositories/VehiclesRepository";
+import { VehicleNotFound } from "../domain/errors/VehicleNotFound";
+
+export class UpdateVehicle {
+  constructor(
+    private vehiclesRepository: Pick<VehiclesRepository, "update" | "getById">
+  ) {}
+
+  async execute(id: string, data: Partial<Omit<VehicleProps, "id">>) {
+    const vehicle = await this.vehiclesRepository.getById(id);
+
+    if (!vehicle) {
+      throw new VehicleNotFound();
+    }
+
+    vehicle.update(data);
+    await this.vehiclesRepository.update(id, vehicle);
+    return vehicle;
+  }
+}
