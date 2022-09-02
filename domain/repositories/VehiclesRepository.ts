003e4548b2cae51e3e87850bac4002c9ffcea1aa
Fernando Souza <fernando.souza@doxacode.com.br>
2022-09-02 02:33:39 -0300

    refactory: add null in get by id method repository vehicles

--- domain/repositories/VehiclesRepository.ts
+++ domain/repositories/VehiclesRepository.ts
@@ -2,7 +2,7 @@ import { Vehicle } from "../entities/Vehicle";
 
 export interface VehiclesRepository {
   save(vehicle: Vehicle): Promise<void>;
-  getById(id: string): Promise<Vehicle>;
+  getById(id: string): Promise<Vehicle | null>;
   list(): Promise<Vehicle[]>;
   update(id: string, vehicle: Vehicle): Promise<void>;
   remove(id: string): Promise<void>;
